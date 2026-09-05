bb949a78d9373664aad37eb11624abbec80e91fe
Kirekq <tishin330@gmail.com>
2026-09-06 01:42:56 +0300

    add H solution

---
+++ Sort/H.py
@@ -0,0 +1,24 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def create(n):
+    points = []
+    for i in range(n):
+        x, y = map(int, input().split())
+        p = Point(x, y)
+        points.append(p)
+    return points
+
+def sort(points, n):
+    for i in range(1, n):
+        for j in range(n - i):
+            if points[j].x**2 + points[j].y**2 > points[j + 1].x**2 + points[j + 1].y**2:
+                points[j], points[j + 1] = points[j + 1], points[j]
+
+n = int(input())
+points = create(n)
+sort(points, n)
+for p in points:
+    print(f"{p.x} {p.y}")
